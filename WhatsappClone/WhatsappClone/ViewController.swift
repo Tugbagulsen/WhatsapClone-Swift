8ccbbb462fb773e74be70814ff57d0b734e8804b
Tuğba <56814718+Tugbagulsen@users.noreply.github.com>
2022-12-05 11:47:06 +0300

    ok

---
+++ WhatsappClone/WhatsappClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsappClone
+//
+//  Created by Tugba Gulsen on 12/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
